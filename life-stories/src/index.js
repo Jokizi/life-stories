40d2af27aa1f374a34a82f9622cb31240dfcd23b
Jokizi <72267511+Jokizi@users.noreply.github.com>
2022-04-20 17:55:35 +0200

    redux instal + getPosts

--- life-stories/src/index.js
+++ life-stories/src/index.js
@@ -2,10 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.scss";
 import App from "./App";
+import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import rootReducer from "./reducers";
+
+// Redux
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
