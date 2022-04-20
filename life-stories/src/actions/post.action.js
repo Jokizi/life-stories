a93577d2c4e66a9104177efccd6fb7f8b9204ec1
Jokizi <72267511+Jokizi@users.noreply.github.com>
2022-04-20 18:14:25 +0200

    addPost

--- life-stories/src/actions/post.action.js
+++ life-stories/src/actions/post.action.js
@@ -1,8 +1,9 @@
-import { getDocs } from "firebase/firestore";
+import { getDocs, addDoc } from "firebase/firestore";
 import { collection } from "firebase/firestore";
 import { db } from "../utils/firebase.config";
 
 export const GET_POSTS = "GET_POSTS";
+export const ADD_POST = "ADD_POSTS";
 
 export const getPosts = () => {
   return (dispatch) => {
@@ -12,6 +13,16 @@ export const getPosts = () => {
           type: GET_POSTS,
           payload: res.docs.map((doc) => ({ ...doc.data(), id: doc.id })),
         });
+      })
+      .catch((err) => console.log(err));
+  };
+};
+
+export const addPost = (data) => {
+  return (dispatch) => {
+    return addDoc(collection(db, "posts"), data)
+      .then(() => {
+        dispatch({ type: ADD_POST, payload: data });
       })
       .catch((err) => console.log(err));
   };
